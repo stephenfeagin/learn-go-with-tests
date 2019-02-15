6677f9a124ffa3dc99c0c66a18c064209d538b9f
Stephen Feagin <sfeagin@ctrise.org>
2019-02-15 17:29:10 +0000

    Pass tests for arbitrary length input in part 4 - arrays

--- GoFundamentals/04_arrays/sum_test.go
+++ GoFundamentals/04_arrays/sum_test.go
@@ -3,12 +3,13 @@ package main
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+		got := Sum(numbers)
+		want := 6
 
-	got := Sum(numbers)
-	want := 15
-
-	if want != got {
-		t.Errorf("got %d, but want %d given %v", got, want, numbers)
-	}
+		if got != want {
+			t.Errorf("got %d, but want %d given %v", got, want, numbers)
+		}
+	})
 }
